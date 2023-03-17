a6789f20f3fe0cb571dfdcf5dd411b39d7d299ad
Jibaru <ignacioruedaboada@hotmail.com>
2023-03-17 14:07:06 -0500

    feat: update presentation

--- ts/presentation/line-items-view.ts
+++ ts/presentation/line-items-view.ts
@@ -2,7 +2,10 @@ import { LineItem } from "../models/line-item";
 
 const lineRows = document.getElementById("line-rows");
 
-export const RenderLineItem = (lineItem: LineItem) => {
+export const RenderLineItem = (
+  lineItem: LineItem,
+  decimalsQuantity: number
+) => {
   const tr = document.createElement("tr");
 
   let td = document.createElement("td");
@@ -15,52 +18,61 @@ export const RenderLineItem = (lineItem: LineItem) => {
 
   td = document.createElement("td");
   td.textContent = `${lineItem.unitPrice.toFixed(
-    4
-  )} (${lineItem.startingUnitPrice.toFixed(4)})`;
+    decimalsQuantity
+  )} (${lineItem.startingUnitPrice.toFixed(decimalsQuantity)})`;
   tr.appendChild(td);
 
   td = document.createElement("td");
-  td.textContent = lineItem.unitValue.toFixed(4);
+  td.textContent = lineItem.unitValue.toFixed(decimalsQuantity);
   tr.appendChild(td);
 
   td = document.createElement("td");
   td.textContent = `${lineItem.igvTotal.toFixed(
-    4
-  )} (${lineItem.igvPercentage.toFixed(4)}%)`;
+    decimalsQuantity
+  )} (${lineItem.igvPercentage.toFixed(decimalsQuantity)}%)`;
   tr.appendChild(td);
 
   td = document.createElement("td");
   td.textContent = `${lineItem.iscTotal.toFixed(
-    4
-  )} (${lineItem.iscPercentage.toFixed(4)}%)`;
+    decimalsQuantity
+  )} (${lineItem.iscPercentage.toFixed(decimalsQuantity)}%)`;
   tr.appendChild(td);
 
   td = document.createElement("td");
   td.textContent = `${lineItem.rcTotal.toFixed(
-    4
-  )} (${lineItem.rcPercentage.toFixed(4)}%)`;
+    decimalsQuantity
+  )} (${lineItem.rcPercentage.toFixed(decimalsQuantity)}%)`;
+  tr.appendChild(td);
+
+  td = document.createElement("td");
+  td.textContent = `${lineItem.icbperTotal.toFixed(
+    decimalsQuantity
+  )} (${lineItem.icbperUnitAmount.toFixed(decimalsQuantity)} x u)`;
   tr.appendChild(td);
 
   td = document.createElement("td");
   td.textContent = `${lineItem.discount.toFixed(
-    4
-  )} (${lineItem.startingDiscount.toFixed(4)})`;
+    decimalsQuantity
+  )} (${lineItem.startingDiscount.toFixed(decimalsQuantity)})`;
   tr.appendChild(td);
 
   td = document.createElement("td");
-  td.textContent = lineItem.subtotal.toFixed(4);
+  td.textContent = lineItem.subtotal.toFixed(decimalsQuantity);
   tr.appendChild(td);
 
   td = document.createElement("td");
-  td.textContent = lineItem.total.toFixed(4);
+  td.textContent = lineItem.total.toFixed(decimalsQuantity);
   tr.appendChild(td);
 
   lineRows.appendChild(tr);
 };
 
-export const ReRenderLineItems = (lineItems: LineItem[]) => {
+export const ReRenderLineItems = (
+  lineItems: LineItem[],
+  decimalsQuantity: number
+) => {
   lineRows.innerHTML = "";
   for (const lineItem of lineItems) {
-    RenderLineItem(lineItem);
+    RenderLineItem(lineItem, decimalsQuantity);
   }
 };
--- ts/presentation/summary-view.ts
+++ ts/presentation/summary-view.ts
@@ -1,23 +1,50 @@
 import { Summary } from "../models/summary";
 
-const totalIgvTd: HTMLElement | null = document.getElementById("total-igv");
-const totalIscTd: HTMLElement | null = document.getElementById("total-isc");
+const totalExoneratedTd: HTMLElement | null =
+  document.getElementById("total-exonerated");
+const totalUnaffectedTd: HTMLElement | null =
+  document.getElementById("total-unaffected");
+const totalIgvWithoutDescTd: HTMLElement | null = document.getElementById(
+  "total-igv-without-desc"
+);
+const totalIgvWithDescTd: HTMLElement | null = document.getElementById(
+  "total-igv-with-desc"
+);
+const totalIscWithoutDescTd: HTMLElement | null = document.getElementById(
+  "total-isc-without-desc"
+);
+const totalIscWithDescTd: HTMLElement | null = document.getElementById(
+  "total-isc-with-desc"
+);
 const totalRcTd: HTMLElement | null = document.getElementById("total-rc");
+const totalIcbperTd: HTMLElement | null =
+  document.getElementById("total-icbper");
 const totalLineDiscountTd: HTMLElement | null = document.getElementById(
   "total-line-discount"
 );
 const totalGlobalDiscountTd: HTMLElement | null = document.getElementById(
   "total-global-discount"
 );
-const subtotalTd: HTMLElement | null = document.getElementById("subtotal");
+const totalTaxedTd: HTMLElement | null = document.getElementById("total-taxed");
 const totalTd: HTMLElement | null = document.getElementById("total");
 
-export const RenderSummary = (summary: Summary) => {
-  totalIgvTd.innerHTML = summary.totalIGV.toFixed(4);
-  totalIscTd.innerHTML = summary.totalISC.toFixed(4);
-  totalRcTd.innerHTML = summary.totalRC.toFixed(4);
-  totalLineDiscountTd.innerHTML = summary.totalLineDiscount.toFixed(4);
-  totalGlobalDiscountTd.innerHTML = summary.totalGlobalDiscount.toFixed(4);
-  subtotalTd.innerHTML = summary.subtotal.toFixed(4);
-  totalTd.innerHTML = summary.total.toFixed(4);
+export const RenderSummary = (summary: Summary, decimalsQuantity: number) => {
+  totalExoneratedTd.innerHTML =
+    summary.totalExonerated.toFixed(decimalsQuantity);
+  totalUnaffectedTd.innerHTML =
+    summary.totalUnaffected.toFixed(decimalsQuantity);
+  totalIgvWithDescTd.innerHTML = summary.totalIGV.toFixed(decimalsQuantity);
+  totalIgvWithoutDescTd.innerHTML =
+    summary.totalIGVWithoutGlobalDiscount.toFixed(decimalsQuantity);
+  totalIscWithDescTd.innerHTML = summary.totalISC.toFixed(decimalsQuantity);
+  totalIscWithoutDescTd.innerHTML =
+    summary.totalISCWithoutGlobalDiscount.toFixed(decimalsQuantity);
+  totalRcTd.innerHTML = summary.totalRC.toFixed(decimalsQuantity);
+  totalIcbperTd.innerHTML = summary.totalICBPER.toFixed(decimalsQuantity);
+  totalLineDiscountTd.innerHTML =
+    summary.totalLineDiscount.toFixed(decimalsQuantity);
+  totalGlobalDiscountTd.innerHTML =
+    summary.totalGlobalDiscount.toFixed(decimalsQuantity);
+  totalTaxedTd.innerHTML = summary.totalTaxed.toFixed(decimalsQuantity);
+  totalTd.innerHTML = summary.total.toFixed(decimalsQuantity);
 };
